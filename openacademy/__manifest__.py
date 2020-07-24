8c45ada45327644c13dea06872e1e10bf3d97650
MarielaRodriguez777 <mariela_rodriguez52@yahoo.es>
2020-07-24 16:57:36 -0600

    [ADD] fin

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'board']
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -29,8 +29,8 @@
         'security/ir.model.access.csv',
         'views/openacademy.xml',
         'views/partner.xml',
-        #'views/session_board.xml',               
-        #'reports.xml', 
+        'views/session_board.xml',               
+        'reports.xml', 
     ],
     # only loaded in demonstration mode
     'demo': [
